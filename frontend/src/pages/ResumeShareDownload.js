679eb2dfefd5ce89ffefde538932b812823ef227
gidambayiantony <gidambayiantony@gmail.com>
2023-11-01 01:51:23 -0700

    Excellent-CV

--- frontend/src/pages/ResumeShareDownload.js
+++ frontend/src/pages/ResumeShareDownload.js
@@ -1,32 +1,65 @@
-// ResumeShareDownload.js
 import React from 'react';
+import './ResumeShareDownload.css';
 
 const ResumeShareDownload = () => {
-  const handleShare = () => {
-    // Code to initiate sharing functionality (via email, social media, etc.)
-    // Usage: Open a modal to share the resume or provide options to send it via email or generate a shareable link
-    console.log('Resume shared!');
-  };
+    const handlePayment = () => {
+        // Initiate payment processing
+        console.log('Processing payment for resume download and sharing...');
+        // Open a modal or initiate payment gateway integration
+    };
 
-  const handleDownload = (format) => {
-    // Code to handle downloading the resume in the specified format (PDF, DOCX, etc.)
-    // Usage: Trigger a download of the resume in the selected format
-    console.log(`Downloading resume in ${format} format`);
-  };
+    const handleShare = (method) => {
+        // Handle different methods of sharing (email, link generation)
+        if (method === 'email') {
+            console.log('Sending resume to email address...');
+            // Logic to send the resume to the specified email address
+        } else if (method === 'link') {
+            console.log('Generating shareable link to resume...');
+            // Logic to generate a shareable link to the resume
+        }
+    };
 
-  return (
-    <div>
-      <h2>Share and Download Your Resume</h2>
-      <button onClick={handleShare}>Share Resume</button>
-      <div>
-        <h3>Download Resume:</h3>
-        <button onClick={() => handleDownload('PDF')}>Download as PDF</button>
-        <button onClick={() => handleDownload('DOCX')}>Download as DOCX</button>
-        {/* Add more download format options as needed */}
-      </div>
-    </div>
-  );
+    const handleDownload = (format) => {
+        // Handle downloading the resume in different formats
+        console.log(`Downloading resume in ${format} format`);
+        // Trigger a download of the resume in the selected format
+    };
+
+    return (
+        <div className="resume-share-download">
+            <h2>Share and Download Your Resume</h2>
+
+            <button className="pay-button" onClick={handlePayment}>
+                Pay to Download and Share Resume
+            </button>
+
+            <div className="download-buttons">
+                <h3>Download Resume:</h3>
+                <button className="download-button" onClick={() => handleDownload('PDF')}>
+                    Download as PDF
+                </button>
+                <button className="download-button" onClick={() => handleDownload('DOCX')}>
+                    Download as DOCX
+                </button>
+                <button className="download-button" onClick={() => handleDownload('HTML')}>
+                    Download as HTML
+                </button>
+                <button className="download-button" onClick={() => handleDownload('EPUB')}>
+                    Download as EPUB
+                </button>
+            </div>
+
+            <div className="share-buttons">
+                <h3>Share Resume:</h3>
+                <button className="share-button" onClick={() => handleShare('email')}>
+                    Send Resume to Email
+                </button>
+                <button className="share-button" onClick={() => handleShare('link')}>
+                    Generate Shareable Link
+                </button>
+            </div>
+        </div>
+    );
 };
 
 export default ResumeShareDownload;
-
